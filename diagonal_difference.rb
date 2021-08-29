d318c543ada437194645d47b14509f70babf2c2c
kiota-rita <rita-kiota@hotmail.com>
2021-08-29 18:04:48 -0300

    segundo commit

--- diagonal_difference.rb
+++ diagonal_difference.rb
@@ -3,9 +3,9 @@ def diagonalDifference(arr)
     soma2 = 0
     resultado = 0
     
-    arr.each_with_index do |arr, index|
-        soma1 += arr [index]
-        soma2 += arr [-index-1]
+    arr.each_with_index do |arr, index| #percorre o array com o index
+        soma1 += arr [index] #soma o emento do array na posição denominada pelo index
+        soma2 += arr [-index-1] #percorre o array de tras pra frente, soma o emento do array na posição denominada pelo index
     end
     
     resultado = (soma1 - soma2).abs
